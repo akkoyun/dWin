18bd18fb5fb87404c623c43a4f1729d5c6325035
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-01-25 14:36:33 +0300

    Initial update

---
+++ src/dWin_Arduino.cpp
@@ -0,0 +1,23 @@
+/* *******************************************************************************
+ *  Copyright (C) 2014-2022 Mehmet Gunce Akkoyun Can not be copied and/or
+ *	distributed without the express permission of Mehmet Gunce Akkoyun.
+ *
+ *	Library				: dWin HMI LCD Screen Library.
+ *	Code Developer		: Mehmet Gunce Akkoyun (akkoyun@me.com)
+ *	Code Developer		: Recep Senbas (recepsenbas@gmail.com)
+ *
+ *********************************************************************************/
+
+#include <dWin_Arduino.h>
+
+bool dwin::Write_VP(const uint16 _Address, const char *_Data) {
+
+
+
+
+}
+
+// Define Library Class
+dwin LCD;
+
+// 1903
---
+++ src/dWin_Arduino.h
@@ -0,0 +1,45 @@
+/* *******************************************************************************
+ *  Copyright (C) 2014-2022 Mehmet Gunce Akkoyun Can not be copied and/or
+ *	distributed without the express permission of Mehmet Gunce Akkoyun.
+ *
+ *	Library				: dWin HMI LCD Screen Library.
+ *	Code Developer		: Mehmet Gunce Akkoyun (akkoyun@me.com)
+ *	Code Developer		: Recep Senbas (recepsenbas@gmail.com)
+ *
+ *********************************************************************************/
+
+#ifndef __dWin_Arduino__
+#define __dWin_Arduino__
+
+// Define Arduino Library
+#ifndef __Arduino__
+#include <Arduino.h>
+#endif
+
+// Define PGMspace Function
+#include <avr/pgmspace.h>
+
+/**
+ * @brief Main Class
+ */
+class dwin {
+
+	public:
+
+		/**
+		 * @brief Write data to specified VP address.
+		 * @version 01.00.00
+		 * @param _Address Memmory address of dWin
+		 * @param _Data Data to write
+		 * @return true - Succsess
+		 * @return false - Failed
+		 */
+		bool Write_VP(const uint16 _Address, const char *_Data);
+
+	private:
+
+};
+
+extern dwin LCD;
+
+#endif /* defined(__dWin_Arduino__) */
